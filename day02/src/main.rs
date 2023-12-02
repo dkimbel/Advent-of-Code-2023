96d9a18721628705bdcb89c51369eec22cfbfe82
Daniel Kimbel <dk@danielkimbel.com>
2023-12-01 22:30:43 -0700

    Solve day 2 part 1, quick and hacky

---
+++ day02/src/main.rs
@@ -0,0 +1,69 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    // let file = File::open("resources/sample_1").unwrap();
+    let file = File::open("resources/input_1").unwrap();
+    let reader = BufReader::new(file);
+
+    let mut games: Vec<Game> = Vec::new();
+    for line in reader.lines() {
+        let line_content = &line.unwrap();
+        let line_halves = line_content.split(": ").collect::<Vec<_>>();
+        let game_id = line_halves[0].split(" ").collect::<Vec<_>>()[1]
+            .parse::<u32>()
+            .unwrap();
+        let mut rounds: Vec<GameRound> = Vec::new();
+        let round_strs = line_halves[1].split("; ");
+        for round_str in round_strs {
+            let mut num_green = 0;
+            let mut num_blue = 0;
+            let mut num_red = 0;
+            let nums_with_colors = round_str.split(", ");
+            for num_with_color in nums_with_colors {
+                let num_color_split = num_with_color.split(" ").collect::<Vec<_>>();
+                let num_str = num_color_split[0];
+                let color = num_color_split[1];
+                let num = num_str.parse::<u32>().unwrap();
+                match color {
+                    "green" => num_green = num,
+                    "red" => num_red = num,
+                    "blue" => num_blue = num,
+                    _ => panic!("Could not match color {}", color),
+                };
+            }
+            let round = GameRound {
+                num_green,
+                num_red,
+                num_blue,
+            };
+            rounds.push(round);
+        }
+        let game = Game {
+            id: game_id,
+            rounds,
+        };
+        games.push(game);
+    }
+    let part_1_matching_games = games.iter().filter(|game| {
+        !game
+            .rounds
+            .iter()
+            .any(|round| round.num_green > 13 || round.num_blue > 14 || round.num_red > 12)
+    });
+    let part_1_solution = part_1_matching_games.map(|game| game.id).sum::<u32>();
+    println!("Part 1 solution: {}", part_1_solution);
+}
+
+#[derive(Debug)]
+struct GameRound {
+    num_green: u32,
+    num_red: u32,
+    num_blue: u32,
+}
+
+#[derive(Debug)]
+struct Game {
+    id: u32,
+    rounds: Vec<GameRound>,
+}
