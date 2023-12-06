2a8ce80a71b10d1e9a7e113cb0727cb8710ef78e
Daniel Kimbel <dk@danielkimbel.com>
2023-12-05 22:27:32 -0700

    Solve day 6 part 2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -11,42 +11,65 @@ fn main() {
     let file_content = fs::read_to_string("resources/input_1").unwrap();
     let split = file_content.split("\n").collect::<Vec<_>>();
 
-    let times = split[0]
+    // let times = split[0]
+    //     .split_whitespace()
+    //     .skip(1)
+    //     .map(|s| s.parse::<usize>().unwrap())
+    //     .collect::<Vec<_>>();
+    // let distances = split[1]
+    //     .split_whitespace()
+    //     .skip(1)
+    //     .collect::<Vec<_>>()
+    //     .iter()
+    //     .map(|s| s.parse::<usize>().unwrap())
+    //     .collect::<Vec<_>>();
+
+    let race_time_ms = split[0]
         .split_whitespace()
         .skip(1)
-        .map(|s| s.parse::<usize>().unwrap())
-        .collect::<Vec<_>>();
-    let distances = split[1]
+        .collect::<String>()
+        .parse::<usize>()
+        .unwrap();
+    let record_distance_mm = split[1]
         .split_whitespace()
         .skip(1)
-        .collect::<Vec<_>>()
-        .iter()
-        .map(|s| s.parse::<usize>().unwrap())
-        .collect::<Vec<_>>();
+        .collect::<String>()
+        .parse::<usize>()
+        .unwrap();
 
-    let mut races: Vec<Race> = Vec::new();
-    for (time, distance) in zip(times, distances) {
-        races.push(Race {
-            time_ms: time,
-            record_distance_mm: distance,
-        })
-    }
-
-    let mut nums_winning_options: Vec<usize> = Vec::new();
-    for race in races.iter() {
-        let mut possible_distances: Vec<usize> = Vec::new();
-        for holdable_ms in 0..race.time_ms {
-            let speed_mm_per_ms = holdable_ms;
-            possible_distances.push(speed_mm_per_ms * (race.time_ms - holdable_ms));
+    let mut num_winning_options = 0;
+    for holdable_ms in 0..race_time_ms {
+        let speed_mm_per_ms = holdable_ms;
+        let distance = speed_mm_per_ms * (race_time_ms - holdable_ms);
+        if distance > record_distance_mm {
+            num_winning_options += 1;
         }
-        nums_winning_options.push(
-            possible_distances
-                .iter()
-                .filter(|d| **d > race.record_distance_mm)
-                .count(),
-        );
     }
+    println!("Part 2 solution: {num_winning_options}");
 
-    let part_1_solution = nums_winning_options.iter().product::<usize>();
-    println!("Part 1 solution: {part_1_solution}");
+    // let mut races: Vec<Race> = Vec::new();
+    // for (time, distance) in zip(times, distances) {
+    //     races.push(Race {
+    //         time_ms: time,
+    //         record_distance_mm: distance,
+    //     })
+    // }
+
+    // let mut nums_winning_options: Vec<usize> = Vec::new();
+    // for race in races.iter() {
+    //     let mut possible_distances: Vec<usize> = Vec::new();
+    //     for holdable_ms in 0..race.time_ms {
+    //         let speed_mm_per_ms = holdable_ms;
+    //         possible_distances.push(speed_mm_per_ms * (race.time_ms - holdable_ms));
+    //     }
+    //     nums_winning_options.push(
+    //         possible_distances
+    //             .iter()
+    //             .filter(|d| **d > race.record_distance_mm)
+    //             .count(),
+    //     );
+    // }
+
+    // let part_1_solution = nums_winning_options.iter().product::<usize>();
+    // println!("Part 1 solution: {part_1_solution}");
 }
